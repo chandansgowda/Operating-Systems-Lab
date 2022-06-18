0fdf4cec94898042bba4ee9fe8c1901201f8ed06
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2022-06-18 18:44:58 +0530

    initial commit

---
+++ Process Management/zombie.c
@@ -0,0 +1,38 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  pid_t child_pid, my_pid, parent_pid;
+  child_pid = fork();
+  if (child_pid < 0)
+  {
+  printf("Fork failed. Exiting!\n");
+  exit(0);
+  }
+  if (child_pid == 0)
+  {
+// Child process
+  printf("[CHILD] This is the child process.\n");
+  my_pid = getpid();
+  parent_pid = getppid();
+  printf("[CHILD] My pid is %d\n", my_pid);
+  printf("[CHILD] My parent's pid is %d\n", parent_pid);
+  printf("[CHILD] Exiting.\n");
+  exit(0);
+  }
+  else
+  {
+// Parent process
+  printf("[PARENT] This is the parent process.\n");
+  my_pid = getpid();
+  parent_pid = getppid();
+  printf("[PARENT] My pid is %d\n", my_pid);
+  printf("[PARENT] My parent's pid is %d\n", parent_pid);
+  printf("[PARENT] Sleeping for 10 seconds.\n");
+  sleep(10);
+  printf("[PARENT] Child pid = %d has ended, but it has an entry >
+  }
+return 0;
+}
