8a33ed425152b587ed6a295e9055735d0ca1d05a
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2022-07-20 20:49:55 +0530

    simple code

--- bash scripts/primeNumbers.sh
+++ bash scripts/primeNumbers.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-read -p "Enter starting number>>" num1
-echo 
-read -p "Enter ending number>>" num2
+echo "Prime Numbers"
+read -p "From: " n1
+read -p "To: " n2
 
-while [ $num1 -lt $num2 ]
-	do
+for (( i=$n1; i<=$n2; i++ ))
+do
+	if [ $i -ge 2 ]
+	then
 		flag=0
-		num3=2
-		if [ $num1 -gt 1 ]
-		then
-			while [ $num3 -lt $num1 ]
-				do
- 					if [ $(($num1%$num3)) -eq 0 ]
-					then
-						flag=1
-						break
-					else
-						((num3++))
-					fi
-				done
-			if [ $flag -eq 0 ]
-			then 
-				echo $num1
+		for (( j=2; j<$i; j++ ))
+		do
+			if [ $(( $i%$j )) -eq 0 ]
+			then
+				flag=1
+				break
 			fi
+		done
+		if [ $flag -eq 0 ]
+		then
+			echo -n "$i "
 		fi
-		((num1++))
-		
-	done
+	fi
+done
+				
