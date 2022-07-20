464fd4279551514d9e1ba318bdb5d90b4648aff0
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2022-07-20 19:53:31 +0530

    improved

--- bash scripts/fibonacci.sh
+++ bash scripts/fibonacci.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-n1=0
-n2=1
-echo "Required no. of fibonacci numbers:"
-read n
-echo "First $n fibonacci numbers are:"
-echo
-echo $n1
-echo $n2
-for ((i=0;i<$n;i++))
+
+read -p "Enter n>> " n
+a=0
+b=1
+
+for (( i=0; i<n; i++ ))
 do
-  n3=$((n1+n2))
-  echo $n3
-  n1=$n2
-  n2=$n3
+	echo -n "$a "
+	fn=$(( $a + $b ))
+	a=$b
+	b=$fn
 done
