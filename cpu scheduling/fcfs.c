4c2abf868bbb047e3775c84206ef9d8f895bf47b
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2022-07-21 08:21:00 +0530

    simple code

--- cpu scheduling/fcfs.c
+++ cpu scheduling/fcfs.c
@@ -1,87 +1,72 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct process
-{
-    int pid, at, bt, ct, tt, wt;
-} p;
+struct process {
+	int pid;
+	int at; //arrival time 
+	int wt; //waiting time
+	int bt; //burst time
+	int ct; //completion time
+	int tt; //turnaround time
+};
 
-float tot_tt = 0;
-float tot_wt = 0;
-void sort(struct process list[80], int s)
-{
-    int i, j;
-    struct process temp;
+int n; //number of processes
+struct process p[50]; //list of process structures
 
-    for (i = 0; i < s - 1; i++)
-    {
-        for (j = 0; j < (s - 1 - i); j++)
-        {
-            if (list[j].at > list[j + 1].at)
-            {
-                temp = list[j];
-                list[j] = list[j + 1];
-                list[j + 1] = temp;
-            }
-        }
-    }
-    for (i = 0; i < s; i++)
-    {
-        list[i].ct = list[i].tt = list[i].wt = 0;
-    }
-}
-
-void findtt(p a[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        a[i].tt = a[i].ct - a[i].at;
-        tot_tt += a[i].tt;
-    }
-}
-
-void findwt(p a[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        a[i].wt = a[i].tt - a[i].bt;
-        tot_wt += a[i].wt;
-    }
-}
-
-void findavg(p a[], int n)
-{
-    sort(a, n);
-    int ct = a[0].at + a[0].bt;
-    a[0].ct = ct;
-    for (int i = 1; i < n; i++)
-    {
-        ct += a[i].bt;
-        a[i].ct = ct;
-    }
-    findtt(a, n);
-    findwt(a, n);
-
-    printf("PID\tAT\tBT\tCT\tTT\tWT\n");
-    for (int i = 0; i < n; i++)
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", a[i].pid, a[i].at, a[i].bt, a[i].ct, a[i].tt, a[i].wt);
-    printf("Avg wt = %.2f\nAvg tt = %.2f\n", tot_wt / n, tot_tt / n);
-}
-
-void main()
-{
-    int n;
-    p a[10];
-    printf("Enter the number of process: ");
-    scanf("%d", &n);
-
-    for (int i = 0; i < n; i++)
-    {
-        a[i].pid = i + 1;
-        printf("\nEnter P%d arrival time: ", (i + 1));
-        scanf("%d", &a[i].at);
-        printf("Enter P%d burst time: ", (i + 1));
-        scanf("%d", &a[i].bt);
-    }
-    findavg(a, n);
+void main(){
+	int i,j;
+	float tot_tt,tot_wt;
+		
+	printf("No. of process >> ");
+	scanf("%d",&n);
+	
+	//getting input
+	for(i=0; i<n;i++){
+		printf("Process%d:\n", i+1);
+		printf("Arrival Time: ");
+		scanf("%d",&p[i].at);
+		printf("Burst Time: ");
+		scanf("%d",&p[i].bt);
+		p[i].tt = p[i].wt = p[i].wt = 0; //initializing with 0
+		p[i].pid = i+1; 
+	}
+	
+	//sorting based on arrival time
+	for(i=0;i<n-1;i++){
+		for(j=0;j<n-i-1;j++){
+			if (p[j].at > p[j+1].at){ //soryin based on arrival time
+				struct process temp = p[j];
+				p[j] = p[j+1];
+				p[j+1] = temp;
+			}
+		}
+	}
+	
+	//calculating completion time
+	int ct = p[0].at + p[0].bt;
+	p[0].ct = ct;
+	for(i=1;i<n;i++){
+		ct += p[i].bt;
+		p[i].ct = ct;
+	}
+	
+	//calculating turnaround time and waiting time
+	for(i=0;i<n;i++){
+	
+		//turnaround time
+		p[i].tt = p[i].ct - p[i].at;
+		tot_tt += p[i].tt;
+		
+		//waiting time
+		p[i].wt = p[i].tt - p[i].bt;
+		tot_wt += p[i].wt;
+	}
+	
+	
+	//printing output
+	printf("\nPID\tAT\tBT\tCT\tTT\tWT\n");
+	for (i = 0; i < n; i++)
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i].pid, p[i].at, p[i].bt, p[i].ct, p[i].tt, p[i].wt);
+    printf("Avg waiting time = %.2f\nAvg turnaround time = %.2f\n", tot_wt / n, tot_tt / n);
+	
+	
 }
